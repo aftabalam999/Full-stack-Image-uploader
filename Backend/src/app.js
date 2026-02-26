5d417123c8739958f97cbdfb3433f4d03bd64453
aftabalam999 <course.aftabalam@gmail.com>
2026-02-26 09:28:02 +0530

    Backend complete

---
+++ Backend/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
+
+const app = express();
+const upload = multer({ storage: multer.memoryStorage() }); // used this for storing the file in memory, you can also configure it to store on disk or other storage services like AWS S3, Google Cloud Storage, etc.
+
+app.use(express.json());
+
+app.post("/create-post", upload.single("image"), async (req, res) => {
+  // console.log(req.body);
+  // console.log(req.file);
+  const result = await uploadFile(req.file.buffer);
+
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
+});
+
+app.get('/posts', async (req,res)=>{
+  const posts = await postModel.find();
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts,
+  });
+})
+
+module.exports = app;
